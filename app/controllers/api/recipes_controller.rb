41a76ad8dbcd9248ed65bafe47c0db61076b1a59
brian <riceb53@gmail.com>
2020-07-05 11:21:26 -0400

    allow update action to preserve default values

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -27,12 +27,12 @@ class Api::RecipesController < ApplicationController
     # find the correct recipe
     @recipe = Recipe.find_by(id: params[:id])
     # upate that particular recipe
-    @recipe.title = params[:title]
-    @recipe.chef = params[:chef]
-    @recipe.ingredients = params[:ingredients]
-    @recipe.directions = params[:directions]
-    @recipe.prep_time = params[:prep_time]
-    @recipe.image_url = params[:image_url]
+    @recipe.title = params[:title] || @recipe.title 
+    @recipe.chef = params[:chef] || @recipe.chef    
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.image_url = params[:image_url] || @recipe.image_url
     @recipe.save
     render 'show.json.jb'
   end
