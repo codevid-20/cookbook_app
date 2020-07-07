745bb5457c5dfa528ed8087dad9d523783f45cb4
brian <riceb53@gmail.com>
2020-07-06 21:17:41 -0400

    add model methods for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,26 @@
 class Recipe < ApplicationRecord
+  # def hello
+  #   "hello it is monday"
+  # end
+
+  def ingredients_list
+    ingredients.downcase.split(", ")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+
+    if hours > 0
+      result += "#{hours} hours"
+    end
+
+    if minutes > 0
+      result += " #{minutes} minutes"
+    end
+    result
+  end
 end
+
+
